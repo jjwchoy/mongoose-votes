9886ee27e978bc6afec5ab119e6f9a6195d0375b
Jason Choy <jjwchoy@gmail.com>
2013-03-29 22:42:35 +0000

    Fixed implementation, made up/downvote functions statics, added a highestVoted function

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var ObjectId = require('mongoose').Schema.Types.ObjectId;
 function makeRecordVote(votesName, votersName, weight) {
     weight = weight || 1;
 
-    return function(voterId, callback) {
-        var q = {_id: this._id};
+    return function(objId, voterId, callback) {
+        var q = {_id: objId};
 
         // only match if this voter hasn't already voted for this
         q[votersName] = {
@@ -24,15 +24,15 @@ function makeRecordVote(votesName, votersName, weight) {
             $inc: inc
         };
 
-        this.constructor.update.call(this.constructor, q, u, callback);
+        this.update(q, u, callback);
     };
 };
 
 function makeCancelVote(votesName, votersName, weight) {
     weight = weight || 1;
 
-    return function(voterId, callback) {
-        var q = {_id: this._id};
+    return function(objId, voterId, callback) {
+        var q = {_id: objId};
 
         // only match if this voter has voted for this
         q[votersName] = voterId;
@@ -50,7 +50,7 @@ function makeCancelVote(votesName, votersName, weight) {
             $inc: inc
         };
 
-        this.constructor.update.call(this.constructor, q, u, callback);
+        this.update(q, u, callback);
     };
 };
 
@@ -65,8 +65,8 @@ module.exports = exports = function(schema, options) {
     var upvotesName = options.upvotesName || 'upvotes';
     var upvotersName = options.upvotersName || 'upvoters';
 
-    var upvoteMethodName = options.upvoteMethodName || 'upvote';
-    var cancelUpvoteMethodName = options.cancelUpvoteMethodName || 'cancelUpvote';
+    var upvoteFuncName = options.upvoteFuncName || 'upvote';
+    var cancelUpvoteFuncName = options.cancelUpvoteFuncName || 'cancelUpvote';
 
     // indexed defaults to false
     var indexed = !!options.indexed;
@@ -87,47 +87,123 @@ module.exports = exports = function(schema, options) {
     var upvoteFunc = makeRecordVote(upvotesName, upvotersName);
     var cancelUpvoteFunc = makeCancelVote(upvotesName, upvotersName);
 
-    schema.methods[upvoteMethodName] = upvoteFunc;
-    schema.methods[cancelUpvoteMethodName] = cancelUpvoteFunc;
+    schema.statics[upvoteFuncName] = upvoteFunc;
+    schema.statics[cancelUpvoteFuncName] = cancelUpvoteFunc;
+
+    schema.methods[upvoteFuncName] = function(voterId, callback) {
+        this.constructor[upvoteFuncName].call(this.constructor, this._id, voterId, callback);
+    };
+
+    schema.methods[cancelUpvoteFuncName] = function(voterId, callback) {
+        this.constructor[cancelUpvoteFuncName].call(this.constructor, this._id, voterId, callback);
+    };
 
     if (!disableDownvotes) {
-        var downvoteMethodName = options.downvoteMethodName || 'downvote';
-        var cancelDownvoteMethodName = options.cancelDownvoteMethodName || 'cancelDownvote';
+        var downvoteFuncName = options.downvoteFuncName || 'downvote';
+        var cancelDownvoteFuncName = options.cancelDownvoteFuncName || 'cancelDownvote';
 
         var downvoteFunc = makeRecordVote(downvotesName, downvotersName);
         var cancelDownvoteFunc = makeCancelVote(downvotesName, downvotersName);
 
         // Modify upvote function to cancel any downvotes first
-        schema.methods[upvoteMethodName] = function(voterId, callback) {
+        schema.statics[upvoteFuncName] = function(objId, voterId, callback) {
             var self = this;
-            cancelDownvoteFunc.call(this, voterId, function(err) {
+            cancelDownvoteFunc.call(this, objId, voterId, function(err) {
                 if (err) {
                     callback(err, null);
                 } else {
-                    upvoteFunc.call(self, voterId, callback);
+                    upvoteFunc.call(self, objId, voterId, callback);
                 }
             });
         };
 
-        schema.methods[downvoteMethodName] = function(voterId, callback) {
+        schema.statics[downvoteFuncName] = function(objId, voterId, callback) {
             var self = this;
-            cancelUpvoteFunc.call(this, voterId, function(err) {
+            cancelUpvoteFunc.call(this, objId, voterId, function(err) {
                 if (err) {
                     callback(err, null);
                 } else {
-                    downvoteFunc.call(self, voterId, callback);
+                    downvoteFunc.call(self, objId, voterId, callback);
                 }
             });
         };
 
-        schema.methods[cancelDownvoteMethodName] = cancelDownvoteFunc;
+        schema.statics[cancelDownvoteFuncName] = cancelDownvoteFunc;
+
+        schema.methods[downvoteFuncName] = function(voterId, callback) {
+            this.constructor[downvoteFuncName].call(this.constructor, this._id, voterId, callback);
+        };
+
+        schema.methods[cancelDownvoteFuncName] = function(voterId, callback) {
+            this.constructor[cancelDownvoteFuncName].call(this.constructor, this._id, voterId, callback);
+        };
     }
+
+    var findUpvotesFuncName = options.findUpvotesFuncName || 'findUpvotes';
+    schema.statics[findUpvotesFuncName] = function(voterId, q, callback) {
+        if (!callback) {
+            callback = q;
+            q = {};
+        }
+        q[upvotersName] = voterId;
+        projection = {
+            _id: 1
+        };
+        this.find(q, projection, callback);
+    };
+    
+    if (!disableDownvotes) {
+        var findDownvotesFuncName = options.findDownvotesFuncName || 'findDownvotes';
+        schema.statics[findDownvotesFuncName] = function(voterId, q, callback) {
+            if (!callback) {
+                callback = q;
+                q = {};
+            }
+            q[downvotersName] = voterId;
+            projection = {
+                _id: 1
+            };
+            this.find(q, projection, callback);
+        };
+    }
+
+    var highestVotedFuncName = options.highestVotedFuncName || 'highestVoted';
+    schema.statics[highestVotedFuncName] = function(match, projection, sort, callback) {
+        projection[tallyName] = {
+            $subtract: ['$' + upvotesName, '$' + downvotesName]
+        };
+        sort = sort || {
+            tallyName: -1
+        };
+        this.aggregate([
+            {
+                $match: match
+            },
+            {
+                $project: projection
+            },
+            {
+                $sort: sort
+            },
+            {
+                $limit: 1
+            }
+        ], function(err, res) {
+            if (err) {
+                callback(err);
+            } else if (res.length) {
+                callback(null, res[0]);
+            } else {
+                callback(null, null);
+            }
+        });
+    };
 
     // If the tally name (total count) is different from then upvoters
     // name then we'll add a virtual. If downvoting is enabled, the tally name
     // *must* be different from the upvoters name, otherwise this will not
     // work as intended
-    if (tallyName !== upvotersName) {
+    if (tallyName !== upvotesName) {
         schema.virtual(tallyName).get(function() {
             if (disableDownvotes) {
                 return this.upvotes;
